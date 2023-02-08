387c05575184be32ac7e9d839baf4a08f05400fa
Phya123 <90482389+Phya123@users.noreply.github.com>
2023-02-07 20:59:04 -0500

    Gvars.py update

--- gvars.py
+++ gvars.py
@@ -16,8 +16,8 @@ operEquity = 10000 # defines the target amount per execution ($)
 limitOrderMargin = 0.1 # percentage that defines the offset for the limit orders
 
 # YOUR API KEYS AT ALPACA GO HERE!
-API_KEY = ""
-API_SECRET_KEY = ""
+API_KEY = "PKPAC5J99GX3RV82B66S"
+API_SECRET_KEY = "lbYutjJ5y8TNyq5MwQZmZqRSp3VeebgCdpLuYdXz"
 ALPACA_API_URL = "https://paper-api.alpaca.markets"
 
 # this block checks whether you have your keys written or not
